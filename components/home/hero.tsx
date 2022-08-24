62c6b531810b78dba861bb15e75cc60bdf67ede0
Smaran Ummadishetty <speak2smaran@gmail.com>
2022-08-24 12:36:29 +0530

    added resume :up:

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -90,7 +90,7 @@ const HeroSection = React.memo(() => {
           name="Resume"
           target="_blank"
           rel="noreferrer"
-          href="/Ayush_Resume.pdf"
+          href="/smaran2022.pdf"
         ></Button>
         <Button
           classes="ml-3"
