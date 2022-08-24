4fe8d1de69e299f585dcb2acc7e0d77d47fdc5c9
Smaran Ummadishetty <speak2smaran@gmail.com>
2022-08-24 12:55:17 +0530

    updated chaport appID :rocket:

--- components/common/scripts.tsx
+++ components/common/scripts.tsx
@@ -23,7 +23,7 @@ const Scripts = React.memo(() => {
             setTimeout(() => {
                 (function (w, d, v3) {
                     w.chaportConfig = {
-                        appId: '5ec95b5d1db3487e521123ca'
+                        appId: '62fcf5e3a46c8b85c4082d90'
                     };
     
                     if (w.chaport) return; v3 = w.chaport = {}; v3._q = []; v3._l = {}; v3.q = function () { v3._q.push(arguments) }; v3.on = function (e, fn) { if (!v3._l[e]) v3._l[e] = []; v3._l[e].push(fn) }; var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://app.chaport.com/javascripts/insert.js'; var ss = d.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss)
